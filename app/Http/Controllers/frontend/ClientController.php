069ea99d2b71c97802032d3b9aaacd848f07dd76
Yogesh5454573 <bachutey@gmail.com>
2025-10-29 21:23:45 +0530

    style: Format code in ClientController for improved readability

--- app/Http/Controllers/frontend/ClientController.php
+++ app/Http/Controllers/frontend/ClientController.php
@@ -8,15 +8,15 @@ use App\Models\{Info, Skills, Experence, Service, Project};
 
 class ClientController extends Controller
 {
-   public function home()
+    public function home()
     {
         $info = Info::first();
         $skills = Skills::whereIn('skill_type', ['languages', 'framework'])->get()->groupBy('skill_type');
         $skills_languages = $skills->get('languages', collect());
         $skills_frameworks = $skills->get('framework', collect());
         $experience = Experence::orderBy('id', 'asc')->get();
-        $service = Service::where('status','active')->get();
-        $project = Project::where('status','active')->get();
-        return view('frontend.home',['info' => $info, 'skills_languages' => $skills_languages, 'skills_frameworks' => $skills_frameworks, 'experience' => $experience, 'service'=> $service, 'project'=> $project]);
+        $service = Service::where('status', 'active')->get();
+        $project = Project::where('status', 'active')->get();
+        return view('frontend.home', ['info' => $info, 'skills_languages' => $skills_languages, 'skills_frameworks' => $skills_frameworks, 'experience' => $experience, 'service' => $service, 'project' => $project]);
     }
 }
